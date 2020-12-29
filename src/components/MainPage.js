5c38cf3aba45cf87186cbd2f910b013f20c45eeb
Vince <vincentjvillacorta@gmail.com>
2020-12-28 17:30:29 -0800

    Created site logo

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import InfoCard from './InfoCard'
 import './MainPage.css'
+import logo from '../images/stock-x-assets-logo.png'
 const MainPage = () => {
     return (
-        <div className='card-set'>
-            <InfoCard/>
-            <InfoCard/>
-            <InfoCard/>
+        <div>
+            <img className='logo' src={logo}/>
+            <div className='card-set'>
+                <InfoCard/>
+                <InfoCard/>
+                <InfoCard/>
+            </div>
         </div>
+        
     )
 }
 
